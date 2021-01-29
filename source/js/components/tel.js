1b71b11030204b657b6cd69a8b9d5170da2265c8
Gennadiy <mr.gennadiy.maksimov@gmail.com>
2021-01-29 15:01:59 +0300

    start form

--- source/js/components/tel.js
+++ source/js/components/tel.js
@@ -2,7 +2,7 @@ import Inputmask from "inputmask";
 
 
 var phone = function () {
-    var im = new Inputmask(`+7 (999) 999 - 999`);
+    var im = new Inputmask(`+7 (999) 999 - 9999`);
     im.mask(`.js-phone`);
 };
   
