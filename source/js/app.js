0a509a5eb1b2c7211b96f69a390a3aa3b4ae2710
Gennadiy <mr.gennadiy.maksimov@gmail.com>
2021-01-30 23:21:54 +0300

    repack

--- source/js/app.js
+++ source/js/app.js
@@ -5,6 +5,7 @@ import firstBlock from './components/first-block.js';
 import menu from './components/menu.js';
 import headerScroll from './components/header.js';
 import products from './components/products.js';
+import bb from './components/bb-slider.js';
 import phone from './components/tel.js';
 import form from './components/form.js';
 import popup from './components/popup.js';
@@ -17,4 +18,5 @@ window.onload = function () {
   products();
   phone();
   form();
+  bb();
 };
---
+++ source/js/components/bb-slider.js
@@ -0,0 +1,51 @@
+import Swiper from 'swiper';
+
+var bb = function () {
+  var Selector = {
+    BB: '.js-bb',
+    BB_SLIDER: '.swiper-container',
+    SWIPER_PAGINATION: '.products__swiper-pagination',
+    nextEl: '.bb-slider__btn_prev',
+    prevEl: '.bb-slider__btn_next',
+  };
+
+  var Class = {
+    BULLET: 'bullet',
+    BULLET_ACTIVE: 'bullet--active',
+  };
+
+  var bb = document.querySelector(Selector.BB);
+
+  if (!bb) {
+    return false;
+  }
+
+  var swiperPagination = bb.querySelectorAll(Selector.SWIPER_PAGINATION);
+  var bbSlider = bb.querySelector(Selector.BB_SLIDER);
+  var btnPrev = bb.querySelector(Selector.nextEl);
+  var btnNext = bb.querySelector(Selector.prevEl);
+
+  var bbSwiper = new Swiper(bbSlider, {
+    keyboard: true,
+    simulateTouch: false,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: btnPrev,
+      prevEl: btnNext,
+    },
+    pagination: {
+      el: swiperPagination,
+      clickable: true,
+      bulletClass: Class.BULLET,
+      bulletActiveClass: Class.BULLET_ACTIVE,
+    },
+    a11y: {
+      paginationBulletMessage: 'Перейти к слайду {{index}}',
+    },
+  });
+
+  return bbSwiper;
+};
+
+export default bb;
